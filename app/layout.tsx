44ede0d7c5f21ffe97d8bf45afb41cac7429b6df
OhYongUn <dhdyddjs@naver.com>
2024-03-26 20:12:20 +0900

    Server Side

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import Navigation from "../components/navigation";
+import SalesLayout from "./about-us/company/jobs/sales/layout";
+import Sales from "./about-us/company/jobs/sales/page";
+import AboutUsLayout from "./about-us/layout";
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: {
+        template: "%s | Next Movies",
+        default: "Next Movies",
+    },
+    description: "The best movies on the best framework",
+};
+export default function Layout({children}: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+        <body>
+        <Navigation/>
+        {children}
+        </body>
+        </html>
+    );
+}
